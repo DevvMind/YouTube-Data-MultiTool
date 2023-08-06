23d4a5c19ec575085e5792a37ba0a83dfcef0ccf
DevvMind <132223099+DevvMind@users.noreply.github.com>
2023-08-06 15:24:18 +0200

    Add files via upload

---
+++ live yt sub count/script.js
@@ -0,0 +1,38 @@
+const API_KEY = 'AIzaSyA6DJD3MgPw-exPLDnjh7H_CgEl_yIke5k';
+let CHANNEL_ID = '';
+const REFRESH_RATE_MS = 5000; // Refresh rate in milliseconds (5 seconds in this example)
+
+function getSubscriberCount() {
+  fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      const subscriberCount = data.items[0].statistics.subscriberCount;
+      document.getElementById('subscriber-count').textContent = subscriberCount;
+    })
+    .catch(error => {
+      console.error('Error fetching data: ', error);
+    });
+}
+
+function getChannelName() {
+  fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${CHANNEL_ID}&key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      const channelName = data.items[0].snippet.title;
+      document.getElementById('channel-name').textContent = channelName;
+    })
+    .catch(error => {
+      console.error('Error fetching data: ', error);
+    });
+}
+
+// Initial call to get the channel name and subscriber count
+getChannelName();
+getSubscriberCount();
+
+// Periodically update the subscriber count every REFRESH_RATE_MS
+setInterval(getSubscriberCount, REFRESH_RATE_MS);
+
+const urlParams = new URLSearchParams(window.location.search);
+const ref = urlParams.get('ref');
+console.log('Referral source:', ref);
